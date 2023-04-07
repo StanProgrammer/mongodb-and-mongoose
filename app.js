c2d4b6bd246a64548748905145809bb3724ca6a9
AtibProgrammer <atibkhan392@gmail.com>
2023-04-07 16:39:22 +0530

    Conected to Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('642fa7dc36cfd0db6f5b448e')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('642fa7dc36cfd0db6f5b448e')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,8 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://atikhan:0rsYoszLRAZiJ7O1@cluster0.e91g1kn.mongodb.net/?retryWrites=true&w=majority'
-  )
+    'mongodb+srv://atikhan:0rsYoszLRAZiJ7O1@cluster0.e91g1kn.mongodb.net/', { dbName: 'shopDB' })
   .then(result => {
     app.listen(3000);
   })
